567b18d0bbde55dd0650520e2e2fe5673eeddcae
Sven A. Schmidt <sas@finestructure.co>
2019-10-10 12:04:07 +0200

    Open up version range

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["ValueCodable"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/jpsim/Yams.git", from: "1.0.0"),
+        .package(url: "https://github.com/jpsim/Yams.git", "1.0.0"..<"3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
