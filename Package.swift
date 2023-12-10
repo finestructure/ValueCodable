ca5317d07159399bdce07a48551422bfa1ad380f
Sven A. Schmidt <sas@finestructure.co>
2023-12-10 20:38:56 +0100

    Package update

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
-// swift-tools-version:4.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.8
 
 import PackageDescription
 
 let package = Package(
     name: "ValueCodable",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ValueCodable",
             targets: ["ValueCodable"]),
@@ -15,8 +13,6 @@ let package = Package(
         .package(url: "https://github.com/jpsim/Yams.git", "1.0.0"..<"3.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ValueCodable",
             dependencies: []),
